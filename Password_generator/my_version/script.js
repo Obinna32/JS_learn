7e5c70dfa9a869b01c95bc34e36af97ba8b9f60b
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-04-18 01:08:47 +0100

    Wrote the script file to generate Password when the Generate button is clicked

--- Password_generator/my_version/script.js
+++ Password_generator/my_version/script.js
@@ -1,3 +1,41 @@
- function generatePassword(){
-    console.log("Hello Obinna")
- }
+const howLong = document.getElementById("number")
+const shouldIncludeLowercase = document.getElementById("includeLowercase");
+const shouldIncludeUppercase = document.getElementById("includeUppercase");
+const shouldIncludeNumbers = document.getElementById("includeNumbers");
+const shouldIncludeSymbol = document.getElementById("includeSymbols");
+const result = document.getElementById("result");
+
+function generatePassword() {
+
+    const length = Number(howLong.value)
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += shouldIncludeLowercase.checked ? lowercaseChars : "";
+    allowedChars += shouldIncludeUppercase.checked ? uppercaseChars : "";
+    allowedChars += shouldIncludeNumbers.checked ? numberChars : "";
+    allowedChars += shouldIncludeSymbol.checked ? symbolChars : "";
+
+    if(length <= 0){
+        window.alert("password length must be at least 11");
+        return;
+    }
+    if(allowedChars.length === 0){
+        window.alert("At least 1 set of character needs to be selected`");
+        return;
+    }
+
+    for(let i=0; i<length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    result.textContent = password;
+
+
+}

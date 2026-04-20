7a6c9251002c1b06e9389e745b16689715534b49
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-04-20 03:42:48 +0100

    Started a Stop watch project using JavaSript

---
+++ Stopwatch/script.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if (!isRunning){
+        startTime = Date.now() - elapsedTime
+    }
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+function update(){
+
+}
